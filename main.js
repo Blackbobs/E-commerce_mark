55151ba7be3bc6c8396bdcfa44faad1d64ed1d3a
Black Bobs <ayodejiayodele350@gmail.com>
2023-06-28 13:32:41 +0100

    E hard

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const cartUi = document.querySelector(".cart-overlay");
 const search = document.querySelector(".fa-search");
 const closeBtn = document.querySelector(".fa-close");
 const closeCart = document.querySelector(".fa-times-circle");
+const productDom = document.querySelector(".inner-products");
 
 // events listener
 menuBtn.addEventListener("click", displayMenu);
@@ -29,13 +30,48 @@ class Products {
     try {
       let result = await fetch("products.json");
       let data = await result.json();
-      return data;
+      let products = data.items;
+      products = products.map((item) => {
+        const title = item.name_d;
+        const price = item.price;
+        const id = item.id;
+        const image = item.img;
+        return { title, price, id, image };
+      });
+      return products;
     } catch (error) {}
   }
 }
 
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach((product) => {
+      result += `
+      <div class="item">
+      <img
+        src=${product.image}
+        alt=""
+        class="img"
+      />
+      <div class="net">
+        <h4 class="name">${product.title}</h4>
+        <i class="fa fa-heart" aria-hidden="true"></i>
+      </div>
+      <div class="order" data-id=${product.id}>
+        <small>${product.price}</small>
+        <a href="#">Add to cart</a>
+      </div>
+    </div>
+      `;
+    });
+    productDom.innerHTML = result;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
   const products = new Products();
 
-  products.getProducts().then((data) => console.log(data));
+  products.getProducts().then((products) => ui.displayProducts(products));
 });
